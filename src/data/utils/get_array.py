def000b5c7f07af6b1e2c412312ef4ab8e38cd5f
gummz <gb9@pm.me>
2022-03-21 16:31:44 +0100

    Add get_array util function

---
+++ src/data/utils/get_array.py
@@ -0,0 +1,30 @@
+import torch
+from tifffile.tifffile import TiffFile
+imoprt numpy as np
+
+
+def get_array(file_path, dimensions, every_second=True, sample=0.01):
+    '''
+    Returns an array of the full data, i.e., 
+    without Maximal Intensity Projection.
+    '''
+    timepoints = dimensions[0]
+    # z-dimension
+    D = dimensions[1]
+    num_samples = int(timepoints*sample)
+
+    with TiffFile(file_path) as f:
+        # If only every second image contains beta cells
+        if every_second:
+            pages = f.pages[::2]
+        else:
+            pages = f.pages
+
+        images = [page.asarray()[0, :, :] for page in pages]
+        images = torch.tensor(images, dtype=torch.uint8)
+        images = images.view((timepoints, D))
+        idx = torch.multinomial(num_samples=num_samples,
+                                replacement=False)
+        images = images[idx]
+
+        return images
