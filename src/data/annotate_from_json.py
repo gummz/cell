598c59bd5f45f7568ff70d30d6df580ab21e5f2b
Gudmundur <gb9@pm.me>
2022-03-30 16:02:56 +0200

    Algorithm written in text

--- src/data/annotate_from_json.py
+++ src/data/annotate_from_json.py
@@ -5,8 +5,38 @@ import os
 from os import listdir
 from os.path import join
 from time import time
+import PIL.Image
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
 
 import src.data.constants as c
+
+'''
+There will be folders inside masks_test called 'filename_json'
+Although I will open the images contained in imgs_test,
+the process (annotation) means I will save the result in masks_test.
+
+The algorithm will walk through every subfolder called
+'filename_json', fetch the 'label.png' in each subfolder,
+retrieve the image index (it is the subfolder name, 'filename_json'), and merge it with the .npy files present in
+masks_test.
+The manual annotation will be processed: grayscale, threshold.
+
+imgs_test:
+_00000.png
+00000.npy
+etc.
+
+masks_test:
+(folder): _00000_json (from labelme script)
+00000.npy (automatic annotations)
+
+masks_test_full:
+00000.npy (manual and automatic annotation together)
+
+This algorithm will be applied to the folder `imgs` in the future. So: imgs, masks, masks_full.
+'''
 
 # Set working directory to script location
 abspath = os.path.abspath(__file__)
@@ -17,6 +47,10 @@ files = c.RAW_FILES_GENERALIZE
 kernel = c.MEDIAN_FILTER_KERNEL
 threshold = c.SIMPLE_THRESHOLD
 
+img_dir = c.IMG_DIR_TEST
+mask_dir = c.MASK_DIR_TEST
+mask_dir_full = c.MASK_DIR_TEST_FULL
+
 # Create folder in case it doesn't exist yet
 folder_name = c.MASK_DIR_TEST
 folder = join(c.DATA_DIR, folder_name)
@@ -25,7 +59,7 @@ try:
 except FileExistsError:
     pass
 try:
-    os.mkdir(FIG_DIR)
+    os.mkdir(c.FIG_DIR)
 except FileExistsError:
     pass
 
@@ -53,6 +87,39 @@ idx = img_idx if img_idx > 0 else 0  # numbering for images
 thresholds = []
 idx = 0
 tic = time()
+manual_labels = []
+auto_labels = []
+for folders, subfolders, files in os.walk(join(c.DATA_DIR, mask_dir)):
+    for file in files:
+        if '.npy' in file:
+            img_idx = file[0:5]
+            load_path = join(c.DATA_DIR, mask_dir)
+            auto_img = np.load(join(load_path, f'{img_idx}.npy'))
+            if len(auto_img.shape) > 2:
+                print(file)
+                print(auto_img.shape)
+                
+                
+        if file == 'label.png':
+            img_idx = folders.split('_')[-2]
+            # print(img_idx)
+
+            path = join(folders, file)
+            # manual_img = PIL.Image.open(path)
+            # manual_img = np.array(manual_img)
+            load_path = join(c.DATA_DIR, mask_dir)
+            auto_img = np.load(join(load_path, f'{img_idx}.npy'))
+            # print(manual_img.shape)
+            # print(auto_img.shape)
+            # added = np.max(np.array([manual_img, auto_img], dtype=object))
+
+            # plt.imsave(f'auto_img_{file}.png', auto_img)
+            # print(manual_img.shape, auto_img.shape)
+
+
+print(*manual_labels)
+
+exit()
 for i, path in enumerate(image_paths):
     label_file = LabelFile(path)
     # img = img_b64_to_arr(label_file.imageData)
@@ -64,13 +131,13 @@ for i, path in enumerate(image_paths):
     q = np.frombuffer(img_data, dtype=np.float32)
     print(q.shape)
 
-    q = q.reshape((1024,1024,38))
+    q = q.reshape((1024, 1024, 38))
     # print(len(img_data))
     # img_pil = Image.open(f)
     # print(img_pil.shape)
     exit()
     img = np.moveaxis(img, 2, 0)
- 
+
     for i in range(4):
         plt.subplot(4, 1, i+1)
         print(img[i].shape)
@@ -78,26 +145,26 @@ for i, path in enumerate(image_paths):
     plt.savefig(join(FIG_DIR, 'test.png'))
 
     # Convert to PIL image; requirement for the model
-    img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     print(img.shape)
-    img=cv2.normalize(img, None, alpha=0, beta=255,
+    img = cv2.normalize(img, None, alpha=0, beta=255,
                         dtype=cv2.CV_8UC1, norm_type=cv2.NORM_MINMAX)
-    name=f'{idx:05d}'
+    name = f'{idx:05d}'
     # Folder to save image to
-    save=join(folder, name)
-    idx=idx + 1
+    save = join(folder, name)
+    idx = idx + 1
 
-    filtered=cv2.medianBlur(img, kernel)
-    _, thresh=cv2.threshold(filtered,
+    filtered = cv2.medianBlur(img, kernel)
+    _, thresh = cv2.threshold(filtered,
                               threshold, 255, cv2.THRESH_BINARY)
     np.save(save, thresh)
 
     # Save as .jpg for debugging
     if i % debug_every == 0:
-        dirs=os.path.dirname(save)
-        file=os.path.basename(save)
+        dirs = os.path.dirname(save)
+        file = os.path.basename(save)
         plt.imsave(f'{dirs}/_{file}.jpg', thresh, cmap='gray')
 
 
-toc=time()
+toc = time()
 print(f'annotate_from_json.py complete after {(toc-tic)/60: .1f} minutes.')
