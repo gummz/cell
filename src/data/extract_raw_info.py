ff6faa0109578d1da5ba2cfc4bbafc944b90e344
gummz <gb9@pm.me>
2022-03-31 15:33:01 +0200

    Bug fix

--- src/data/extract_raw_info.py
+++ src/data/extract_raw_info.py
@@ -1,20 +1,21 @@
 import pandas as pd
-from os.path import join
+from os.path import join, splitext
 import src.data.constants as c
-
 c.setcwd()
 
 # Split path into lists, don't take last item
 # so that we  move up one directory, and lastly
 # join items of list into one string with ''.join
 raw_data_folder = '/'.join(c.RAW_DATA_DIR.split('/')[:-1])
+data_folder = '/'.join(c.DATA_DIR.split('/')[:-2])
+print(data_folder)
 save_location = c.DATA_DIR
 excel = pd.read_excel(join(raw_data_folder, c.EXCEL_FILENAME))
 
 excel['Raw data file name'] = excel['Raw data file name'].str.replace(
     ' ', '_').str.replace('.lsm', '')
 excel['Comment'] = excel['Comment'].str.lower()
-
-excel.to_csv(join(c.DATA_DIR, c.EXCEL_FILENAME))
+excel_noext = splitext(c.EXCEL_FILENAME)[0]
+excel.to_csv(join(data_folder, f'{excel_noext}.csv'))
 
 print('extract_raw_info.py complete')
