b22c0dc315e43c2abc7c18836edfb2464bb4a0e2
gummz <gb9@pm.me>
2022-04-04 13:55:54 +0200

    Update extract_sample to test .czi

--- src/data/extract_sample.py
+++ src/data/extract_sample.py
@@ -1,7 +1,19 @@
+from os.path import join
 from pydantic import FilePath
 from src.data.utils.get_array import get_raw_array
-from src.data.constants import RAW_DATA_DIR, TIMEPOINTS_TEST, RAW_FILE_DIMENSIONS_TEST
+import src.data.constants as c
 from os.path import join
+import numpy as np
 
-array = get_raw_array(0, train=False)
+print('start')
+c.setcwd(__file__)
+# file = list(c.RAW_FILES_GENERALIZE.keys())[2]
+file = 'LI_2020-12-10_emb3_pos4.czi'
+# timepoints = c.RAW_FILES_GENERALIZE[file]
+file_path = join(c.RAW_DATA_DIR, file)
+
+array = get_raw_array(file_path, 150)
+save = join(c.DATA_DIR, 'sample.npy')
+np.save(save, array)
+print('Saved to', save)
 print(array.shape)
