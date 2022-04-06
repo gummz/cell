0b367b95ed720578114ef9b8cec2959c15c3c8a7
gummz <gb9@pm.me>
2022-04-06 23:07:01 +0200

    Add make_dataset_train.sh

--- shell/make_dataset_train.sh
+++ shell/make_dataset_train.sh
@@ -3,7 +3,7 @@
 ### -- specify queue -- 
 #BSUB -q hpc
 ### -- set the job Name -- 
-#BSUB -J make_dataset
+#BSUB -J make_dataset_train
 ### -- ask for number of cores (default: 1) -- 
 #BSUB -n 4 
 ### -- specify that the cores must be on the same host -- 
