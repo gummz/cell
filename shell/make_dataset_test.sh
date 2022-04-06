4642e526409418417f507621b35eb9bad45c5f34
gummz <gb9@pm.me>
2022-04-06 23:06:45 +0200

    Change to CPU

--- shell/make_dataset_test.sh
+++ shell/make_dataset_test.sh
@@ -1,17 +1,19 @@
 #!/bin/sh 
 ### General options 
 ### -- specify queue -- 
-#BSUB -q gpuv100
+#BSUB -q hpc
 ### -- set the job Name -- 
 #BSUB -J make_dataset_test
-### -- ask for number of cores (default: 1) --
-#BSUB -n 1
-### -- Select the resources: 1 gpu in exclusive process mode --
-#BSUB -gpu "num=1:mode=exclusive_process"
-### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
-#BSUB -W 9:00
-# request 5GB of system-memory
-#BSUB -R "rusage[mem=9GB]"
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 4 
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 1GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=1GB]"
+### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot -- 
+#BSUB -M 3GB
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 24:00 
 
 ### -- send notification at start -- 
 #BSUB -B 
