e70a184a2bc4a3c48989a815dfb6addb4d1e4bb4
gummz <gb9@pm.me>
2022-05-01 00:57:23 +0200

    Draft of 3D plot script

--- src/visualization/3d_plot.py
+++ src/visualization/3d_plot.py
@@ -2,51 +2,26 @@ from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 from os.path import join
 from os import listdir, makedirs
-import os
 import numpy as np
-
-from src.data.constants import FIG_DIR, RAW_DATA_DIR, RAW_FILE_DIMENSIONS, TIMEPOINTS, RAW_FILES
+import pickle
+import src.data.constants as c
+import src.data.utils.utils as utils
 import tifffile as tiff
 
 
-# Set working directory to file location
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
+utils.setcwd(__file__)
 
 operation = '3d_plot'
-save = join(FIG_DIR, operation)
-try:
-    makedirs(save)
-except FileExistsError:
-    pass
+save = join(c.FIG_DIR, operation)
+utils.make_dir(save)
 
-# files = RAW_FILES[START_IDX:]
-files = listdir(RAW_DATA_DIR)
-# Filter out already used files
-files = [file for file in files if file in RAW_FILES]
-file_paths = [join(RAW_DATA_DIR, file) for file in files]
-file_paths_tiff = [file for file in file_paths if '.lsm' in file]
 
-fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
+def prepare_3d(timepoint):
+    points = np.array(timepoint)
+    return points, get_cmap()
 
-file_idx = 0
-file_path = file_paths_tiff[file_idx]
-with tiff.TiffFile(file_path) as f:
-    pages = np.array(f.pages[::2])
 
-    pages = pages.reshape(
-        (TIMEPOINTS[file_idx], RAW_FILE_DIMENSIONS[files[file_idx]]))
-    timepoint = pages[0, :]
-    timepoint = [slice.asarray()[0, :, :] for slice in timepoint]
-    timepoint = np.array(timepoint)
-    timepoint = np.moveaxis(timepoint, 0, 2)
-    size = 512
-    r = range(size)
-    r38 = np.linspace(0, 1, 38)
-    X, Y, Z = np.meshgrid(r, r, r38)
-    print(X.shape, Y.shape, Z.shape)
-
+def get_cmap():
     # Choose colormap
     cmap = plt.cm.RdBu
 
@@ -59,13 +34,66 @@ with tiff.TiffFile(file_path) as f:
     # Create new colormap
     my_cmap = ListedColormap(my_cmap)
 
-    # X = timepoint[0]
-    # Y = timepoint[1]
-    # Z = timepoint[2]
-    # # ax.plot_surface(mesh)
-    timepoint = np.where(timepoint > 100, timepoint, 0)
-    ax.scatter(X, Y, Z, c=timepoint[0:size, 0:size, :], cmap=my_cmap)
+    return my_cmap
 
-    plt.savefig(join(save, 'plot.jpg'))
+# # files = RAW_FILES[START_IDX:]
+# files = listdir(c.RAW_DATA_DIR)
+# # Filter out already used files
+# files = [file for file in files if file in c.RAW_FILES]
+# file_paths = [join(c.RAW_DATA_DIR, file) for file in files]
+# file_paths_tiff = [file for file in file_paths if '.lsm' in file]
 
-print('3d_plot.py complete')
+# fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
+
+# file_idx = 0
+# file_path = file_paths_tiff[file_idx]
+# with tiff.TiffFile(file_path) as f:
+#     pages = np.array(f.pages[::2])
+
+#     pages = pages.reshape(
+#         (c.TIMEPOINTS[file_idx], c.RAW_FILE_DIMENSIONS[files[file_idx]]))
+#     timepoint = pages[0, :]
+#     timepoint = [slice.asarray()[0, :, :] for slice in timepoint]
+#     timepoint = np.array(timepoint)
+#     timepoint = np.moveaxis(timepoint, 0, 2)
+#     size = 512
+#     r = range(size)
+#     r38 = np.linspace(0, 1, 38)
+#     X, Y, Z = np.meshgrid(r, r, r38)
+#     print(X.shape, Y.shape, Z.shape)
+
+#     # Choose colormap
+#     cmap = plt.cm.RdBu
+
+#     # Get the colormap colors
+#     my_cmap = cmap(np.arange(cmap.N))
+
+#     # Set alpha
+#     my_cmap[:, -1] = np.linspace(0, 1, cmap.N)
+
+#     # Create new colormap
+#     my_cmap = ListedColormap(my_cmap)
+
+#     # X = timepoint[0]
+#     # Y = timepoint[1]
+#     # Z = timepoint[2]
+#     # # ax.plot_surface(mesh)
+#     timepoint = np.where(timepoint > 100, timepoint, 0)
+#     ax.scatter(X, Y, Z, c=timepoint[0:size, 0:size, :], cmap=my_cmap)
+
+#     plt.savefig(join(save, 'plot.jpg'))
+
+
+if __name__ == '__main__':
+    name = 'LI_2019-02-05_emb5_pos3.lsm'
+    file = 'LI_2019-02-05_emb5_pos3.lsm_0_3.csv'
+    chains_tot = np.load(open(join(c.DATA_DIR, c.PRED_DIR, name, file), 'rb'))
+    figures = []
+    for timepoint in chains_tot:
+        fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
+        points, cmap = prepare_3d(timepoint)
+        X, Y, Z = points[:, :3]
+        ax.scatter(X, Y, Z, c=points[:, 3], cmap=cmap)
+        figures.append(fig)
+
+    print('3d_plot.py complete')
