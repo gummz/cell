aa0d546dd917264d67fe31e45929c1440a7fefa2
gummz <gb9@pm.me>
2022-05-01 00:50:40 +0200

    extract_sample now reports index of image in dataset

--- src/data/extract_sample.py
+++ src/data/extract_sample.py
@@ -4,13 +4,14 @@ import pandas as pd
 import src.data.constants as c
 import src.data.utils.utils as utils
 
-index = 45  # '00023'
+index = 88  # '00023'
 mode = 'train'
 custom = False
+resize = False
 # which:
 # Should the extract be one timepoint across slices,
 # or one slice across timepoints?
-which = 'slice'  # or which = 'slice'
+which = 'timepoint'  # or which = 'slice'
 
 utils.setcwd(__file__)
 
@@ -19,7 +20,8 @@ slice_record = pd.read_csv(
     record_path, sep='\t', header=0, dtype={'name': str})
 name, file, t, z = slice_record.iloc[index].values
 if custom:
-    t = 42
+    t = 9
+    z = 11
 raw_file_path = join(c.RAW_DATA_DIR, file)
 
 idx = int(t) if which == 'timepoint' else int(z)
@@ -31,7 +33,7 @@ utils.make_dir(output_path)
 for i, array in enumerate(data):
     name_str = f'{t:02d}_{i:02d}.jpg' if which == 'timepoint' \
         else f'{i:02d}_{z:02d}.jpg'
-    utils.imsave(join(output_path, name_str), array)
+    utils.imsave(join(output_path, name_str), array, resize=resize)
 
 
-print('Extracted from\n\t', file, '\nthe', which, '\n\t', idx, '\nTimepoint\n\t', t, '\nSlice\n\t', z)
+print('Extracted index', index, 'from\n\t', file, '\nthe', which, '\n\t', idx, '\nTimepoint\n\t', t, '\nSlice\n\t', z)
