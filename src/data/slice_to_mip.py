abd0c2f04744157e51edf4587bde37cec2fddace
Gudmundur <gb9@pm.me>
2022-05-03 18:29:48 +0200

    Change slice_to_mip to function

--- src/data/slice_to_mip.py
+++ src/data/slice_to_mip.py
@@ -6,37 +6,45 @@ import pandas as pd
 import src.data.constants as c
 import src.data.utils.utils as utils
 
-mode = 'train'
-resize = False
-# which:
-# Should the extract be one timepoint across slices,
-# or one slice across timepoints?
-which = 'timepoint'  # or which = 'slice'
 
-utils.setcwd(__file__)
+def slice_to_mip(mode, resize, which, ext='jpg'):
+    images = sorted([img for img in listdir(
+        join(c.DATA_DIR, mode, c.IMG_DIR)) if '.npy' in img])
 
-images = sorted([img for img in listdir(
-    join(c.DATA_DIR, mode, c.IMG_DIR)) if '.npy' in img])
+    record_path = join(c.DATA_DIR, mode, c.IMG_DIR, 'slice_record.csv')
+    slice_record = pd.read_csv(
+        record_path, sep='\t', header=0, dtype={'name': str})
 
-record_path = join(c.DATA_DIR, mode, c.IMG_DIR, 'slice_record.csv')
-slice_record = pd.read_csv(
-    record_path, sep='\t', header=0, dtype={'name': str})
+    for image in images:
+        index = int(splitext(image)[0])
+        name, file, t, z = slice_record.iloc[index].values
+        raw_file_path = join(c.RAW_DATA_DIR, file)
 
-for image in images:
-    index = int(splitext(image)[0])
-    name, file, t, z = slice_record.iloc[index].values
-    raw_file_path = join(c.RAW_DATA_DIR, file)
+        idx = int(t) if which == 'timepoint' else int(z)
+        data = utils.get_raw_array(raw_file_path, which, idx)
+        data_mip = np.max(data, axis=0)
+        del data
+        folder = f'{which}s_{mode}'
+        output_path = join(c.DATA_DIR, c.EXTRACT_DIR, folder)
+        utils.make_dir(output_path)
 
-    idx = int(t) if which == 'timepoint' else int(z)
-    data = utils.get_raw_array(raw_file_path, which, idx)
-    data_mip = np.max(data, axis=0)
-    del data
-    folder = f'{which}s_{mode}'
-    output_path = join(c.DATA_DIR, c.EXTRACT_DIR, folder)
-    utils.make_dir(output_path)
+        name_str = f'index_{t:05d}_mip.{ext}'
 
-    name_str = f'index_{t:05d}_mip.jpg'
-    utils.imsave(join(output_path, name_str), data_mip, resize=resize)
+        utils.imsave(join(output_path, name_str), data_mip, resize=resize)
+
+    return output_path
 
 
-print('Performed MIP on all images from', mode, 'and saved in\n\t', output_path)
+if __name__ == '__main__':
+    mode = 'train'
+    resize = False
+    # which:
+    # Should the extract be one timepoint across slices,
+    # or one slice across timepoints?
+    which = 'timepoint'  # or which = 'slice'
+    utils.setcwd(__file__)
+
+    output_path = slice_to_mip(mode, resize, which)
+
+    print('Performed MIP on all images from',
+          mode, 'and saved in\n\t', output_path)
