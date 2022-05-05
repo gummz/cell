76a71a5c92e19a7e135b8650a2d852f7bc67eb12
Gudmundur <gb9@pm.me>
2022-05-05 15:35:44 +0200

    Added bilateral and average filters

--- src/experiments/compare_filters/experiment_compare_filters.py
+++ src/experiments/compare_filters/experiment_compare_filters.py
@@ -15,6 +15,9 @@ from skimage.io import imread, imsave
 import src.data.constants as c
 import src.data.utils.utils as utils
 
+mode = 'train'
+img_idx = 1000
+
 tic = time()
 utils.setcwd(__file__)
 
@@ -22,21 +25,13 @@ DIR = c.RAW_DATA_DIR
 ext = c.IMG_EXT
 files = c.RAW_FILES
 KERNEL = c.MEDIAN_FILTER_KERNEL
-mode = 'train'
 imgs_path = join('..', c.DATA_DIR, mode, c.IMG_DIR)
 filename = os.path.basename(__file__)
 filename = os.path.splitext(filename)[0]
 
-
-figures_dir = c.FIG_DIR
-folder = filename
-modification = ''
-
-
 images = sorted([image for image in listdir(imgs_path) if '.npy' in image])
 # Get full image paths from filename list `images`
 image_paths = sorted([join(imgs_path, image) for image in images])
-img_idx = 1000
 path = image_paths[img_idx]
 img_name = images[img_idx].split('.')[0]
 save = join(c.FIG_DIR, mode, img_name)
@@ -49,66 +44,102 @@ img = cv2.normalize(img, None, alpha=0, beta=255,
                     dtype=cv2.CV_8UC1, norm_type=cv2.NORM_MINMAX)
 # hist = cv2.calcHist([img], [0], None, [256], [0, 256])
 
-
-'''
-
-'''
-
-# Operation
-# Median Blur
-# operation = 'medianblur'
-# for i in range(1, 21, 2):
-#     img_blur = cv2.medianBlur(img, i)
-#     img_blur = np.array(img_blur)
-#     img_blur = np.where(img_blur > 5, img_blur, 0)
-#     cv2.imwrite(f'{save}/{operation}_{img_name}_{i}.{c.IMG_EXT}', img_blur)
-
 cv2.imwrite(join(save, f'img_cv.{ext}'), img)
 plt.imsave(join(save, f'img_plt.{ext}'), img)
 
+# Operation: mean blur
+operation = 'meanblur'
+utils.make_dir(join(save, operation))
+for i in range(1, 21, 2):
+    img_blur = cv2.blur(img, (i, i))
+    # img_blur = np.array(img_blur)
+    # img_blur = np.where(img_blur > 5, img_blur, 0)
+    name = f'{operation}_{i}'
+    utils.imsave(join(save, operation, name), img_blur)
+
+# Operation
+# Median Blur
+operation = 'medianblur'
+utils.make_dir(join(save, operation))
+for i in range(1, 21, 2):
+    img_blur = cv2.medianBlur(img, i)
+    # img_blur = np.array(img_blur)
+    # img_blur = np.where(img_blur > 5, img_blur, 0)
+    name = f'{operation}_{i}'
+    utils.imsave(join(save, operation, name), img_blur)
+
+# Operation: Gaussian blur
+operation = 'gaussianblur'
+utils.make_dir(join(save, operation))
+for kernel_size in [1, 5, 9, 15]:
+    for sigma_x in [1, 5, 9]:
+        for sigma_y in [1, 5, 9]:
+            name = f'{operation}_{kernel_size}_{sigma_x}_{sigma_y}'
+            if os.path.exists(join(save, name)):
+                break
+
+            img_gauss = cv2.GaussianBlur(
+                img, (kernel_size, kernel_size),
+                sigma_x, sigmaY=sigma_y)
+
+            utils.imsave(join(save, operation, name), img_gauss)
+
+
+# Operation: Bilateral filter
+operation = 'bilateral'
+utils.make_dir(join(save, operation))
+for filter_size in [50, 150]:
+    for sigma_color in [50, 150]:
+        for sigma_space in [5, 9]:
+            img_bilateral = cv2.bilateralFilter(
+                img, filter_size, sigma_color, sigma_space)
+            name = f'{operation}_{filter_size}_{sigma_color}_{sigma_space}'
+            utils.imsave(join(save, operation, name), img_bilateral)
+
+
 # Operation
 # Denoise
-# operation = 'denoise'
-# for i in range(1, 21, 2):
-#     for j in range(1, 10, 2):
-#         for k in range(1, 30, 4):
-#             img_denoise = cv2.fastNlMeansDenoising(img, None, i, j, k)
-#             cv2.imwrite(
-#                 f'{save}/{operation}_cv2_{img_name}_{i}_{j}_{k}.{png}', img_denoise)
-#             plt.imsave(
-#                 f'{save}/_TEST_{operation}_plt_{img_name}_{i}_{j}_{k}.{ext}', img_denoise)
+operation = 'denoise'
+utils.make_dir(join(save, operation))
+for i in range(1, 21, 2):
+    for j in range(1, 10, 2):
+        for k in range(1, 30, 4):
+            name = f'{operation}_{i}_{j}_{k}'
+            img_denoise = cv2.fastNlMeansDenoising(img, None, i, j, k)
+            utils.imsave(join(save, operation, name),
+                         img_denoise)
 
-# operation = 'frangi'
-# for alpha in np.linspace(0.1, 1, 10):
-#     for beta in np.linspace(0.1, 1, 10):
-#         for gamma in np.linspace(1, 30, 5):
-#             img_frangi = frangi(img, alpha=alpha, beta=beta,
-#                                 gamma=gamma, black_ridges=False)
-#             name = f'{operation}_plt_{img_name}_{alpha:.2f}_{beta}_{gamma}'
-#             plt.imsave(f'{save}/{name}.{ext}', img_frangi)
+operation = 'frangi'
+utils.make_dir(join(save, operation))
+for alpha in np.linspace(0.1, 1, 10):
+    for beta in np.linspace(0.1, 1, 10):
+        for gamma in np.linspace(1, 30, 5):
+            img_frangi = frangi(img, alpha=alpha, beta=beta,
+                                gamma=gamma, black_ridges=False)
+            name = f'{operation}_plt_{img_name}_{alpha:.2f}_{beta}_{gamma}'
+            utils.imsave(join(save, operation, name), img_frangi)
 
 # TODO: Canny edge detection
-# img_canny_norm = cv2.normalize(img, None, alpha=0, beta=255,
-#                                dtype=cv2.CV_8UC1, norm_type=cv2.NORM_MINMAX)
 operation = 'canny'
+utils.make_dir(join(save, operation))
 for thresh1 in [20, 50, 80, 100, 150, 200][-2:]:
     for thresh2 in [20, 50, 80, 100, 150, 200][-2:]:
         for aperture_size in [3, 5, 7]:
             for L2_gradient in [True, False]:
                 img = cv2.fastNlMeansDenoising(img, None, 11, 7, 21)
-                img = cv2.normalize(img, None, alpha=0,
-                                    beta=1, dtype=cv2.CV_32FC1,
-                                    norm_type=cv2.NORM_MINMAX)
-                img *= np.where((0.05 < img) & (img < 0.3), img * 3, img)
-                img = cv2.normalize(img, None, alpha=0,
-                                    beta=255, dtype=cv2.CV_8UC1,
-                                    norm_type=cv2.NORM_MINMAX)
+                # img = cv2.normalize(img, None, alpha=0,
+                #                     beta=1, dtype=cv2.CV_32FC1,
+                #                     norm_type=cv2.NORM_MINMAX)
+                # img *= np.where((0.05 < img) & (img < 0.3), img * 3, img)
+                # img = cv2.normalize(img, None, alpha=0,
+                #                     beta=255, dtype=cv2.CV_8UC1,
+                #                     norm_type=cv2.NORM_MINMAX)
                 img_canny = cv2.Canny(
                     img, thresh1, thresh2, None,
                     apertureSize=aperture_size, L2gradient=L2_gradient)
                 name = (f'canny_{thresh1}_{thresh2}'
                         f'_{aperture_size}_{L2_gradient}')
-                utils.imsave(join(save, name), img_canny, 512)
+                utils.imsave(join(save, operation, name), img_canny, 512)
 
 # Operation
 # Simple Threshold
@@ -125,12 +156,6 @@ for thresh1 in [20, 50, 80, 100, 150, 200][-2:]:
 
 # cv2.imwrite(f'{save}/{img_name}_{operation}.jpg', bright)
 
-# # Operation
-# # Generate and save histogram
-# operation = 'histogram'
-# plt.hist(img_blur.ravel(), 256, [0, 256])
-# plt.show()
-# plt.savefig(f'{save}/{img_name}_{operation}.jpg')
 
 # # Operation
 # # Generate and save histogram of intensified image
