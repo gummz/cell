e6ef57cd60d68132180f5845c7728f7b16c4458c
gummz <gb9@pm.me>
2022-05-27 15:10:08 +0200

    Preliminary report visualizations

--- src/visualization/report.py
+++ src/visualization/report.py
@@ -1,5 +1,21 @@
-def function1():
-    pass
+from aicsimageio import AICSImage
+import src.data.utils.utils as utils
+import src.data.constants as c
+import os.path as osp
+import numpy as np
+
+
+def building_dataset(file_path, array):
+    fig_dir = osp.join(c.FIG_DIR, 'building_dataset')
+    utils.make_dir(fig_dir)
+
+    array_mip = np.max(array)
+    slices = np.random.randint(low=0,
+                               high=len(array_mip), size=3)
+    array_slices = array[slices]
+    utils.imsave(osp.join(fig_dir, 'MIP'), array_mip)
+    for i, z_slice in enumerate(array_slices):
+        utils.imsave(osp.join(fig_dir, i), z_slice)
 
 
 def function2():
@@ -7,6 +23,10 @@ def function2():
 
 
 if __name__ == '__main__':
-    to_output = [function1, function2]
+    file_path = osp.join(c.RAW_DATA_DIR, c.PRED_FILE)
+    array = utils.get_raw_array(file_path, t=100)
+    args = (file_path, array)
+
+    to_output = [building_dataset, function2]
     for output in to_output:
-        output()
+        output(*args)
