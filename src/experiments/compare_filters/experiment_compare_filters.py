22d26cb11838ca4ab68a8cd05420c754cb8c46c2
gummz <gb9@pm.me>
2022-05-27 15:14:06 +0200

    Minor additions

--- src/experiments/compare_filters/experiment_compare_filters.py
+++ src/experiments/compare_filters/experiment_compare_filters.py
@@ -62,11 +62,29 @@ for i in range(1, 21, 2):
 operation = 'medianblur'
 utils.make_dir(join(save, operation))
 for i in range(1, 21, 2):
+    name = f'{operation}_{i}'
+    if os.path.exists(join(save, operation, name)):
+        break
     img_blur = cv2.medianBlur(img, i)
     # img_blur = np.array(img_blur)
     # img_blur = np.where(img_blur > 5, img_blur, 0)
-    name = f'{operation}_{i}'
     utils.imsave(join(save, operation, name), img_blur)
+
+
+# Operation
+# Denoise
+operation = 'denoise'
+utils.make_dir(join(save, operation))
+for i in range(1, 21, 2):
+    for j in range(1, 10, 2):
+        for k in range(1, 30, 4):
+            name = f'{operation}_{i}_{j}_{k}'
+            if os.path.exists(join(save, operation, name)):
+                break
+            img_denoise = cv2.fastNlMeansDenoising(img, None, i, j, k)
+            utils.imsave(join(save, operation, name),
+                         img_denoise)
+
 
 # Operation: Gaussian blur
 operation = 'gaussianblur'
@@ -75,7 +93,7 @@ for kernel_size in [1, 5, 9, 15]:
     for sigma_x in [1, 5, 9]:
         for sigma_y in [1, 5, 9]:
             name = f'{operation}_{kernel_size}_{sigma_x}_{sigma_y}'
-            if os.path.exists(join(save, name)):
+            if os.path.exists(join(save, operation, name)):
                 break
 
             img_gauss = cv2.GaussianBlur(
@@ -91,29 +109,22 @@ utils.make_dir(join(save, operation))
 for filter_size in [50, 150]:
     for sigma_color in [50, 150]:
         for sigma_space in [5, 9]:
+            name = f'{operation}_{filter_size}_{sigma_color}_{sigma_space}'
+            if os.path.exists(join(save, operation, name)):
+                break
             img_bilateral = cv2.bilateralFilter(
                 img, filter_size, sigma_color, sigma_space)
-            name = f'{operation}_{filter_size}_{sigma_color}_{sigma_space}'
+
             utils.imsave(join(save, operation, name), img_bilateral)
 
-
-# Operation
-# Denoise
-operation = 'denoise'
-utils.make_dir(join(save, operation))
-for i in range(1, 21, 2):
-    for j in range(1, 10, 2):
-        for k in range(1, 30, 4):
-            name = f'{operation}_{i}_{j}_{k}'
-            img_denoise = cv2.fastNlMeansDenoising(img, None, i, j, k)
-            utils.imsave(join(save, operation, name),
-                         img_denoise)
 
 operation = 'frangi'
 utils.make_dir(join(save, operation))
 for alpha in np.linspace(0.1, 1, 10):
     for beta in np.linspace(0.1, 1, 10):
         for gamma in np.linspace(1, 30, 5):
+            if os.path.exists(join(save, operation, name)):
+                break
             img_frangi = frangi(img, alpha=alpha, beta=beta,
                                 gamma=gamma, black_ridges=False)
             name = f'{operation}_plt_{img_name}_{alpha:.2f}_{beta}_{gamma}'
@@ -126,6 +137,8 @@ for thresh1 in [20, 50, 80, 100, 150, 200][-2:]:
     for thresh2 in [20, 50, 80, 100, 150, 200][-2:]:
         for aperture_size in [3, 5, 7]:
             for L2_gradient in [True, False]:
+                if os.path.exists(join(save, operation, name)):
+                    break
                 img = cv2.fastNlMeansDenoising(img, None, 11, 7, 21)
                 # img = cv2.normalize(img, None, alpha=0,
                 #                     beta=1, dtype=cv2.CV_32FC1,
