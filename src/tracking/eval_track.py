981f516b3404ff5ee692faa9fa7ebc9c04e184e0
gummz <gb9@pm.me>
2022-06-13 12:05:09 +0200

    Refactor into smaller functions

--- src/tracking/eval_track.py
+++ src/tracking/eval_track.py
@@ -8,6 +8,7 @@ from time import time
 import cv2
 
 import numpy as np
+import pandas as pd
 import skimage
 import src.data.constants as c
 import src.data.utils.utils as utils
@@ -16,7 +17,7 @@ import src.visualization.utils as viz
 from matplotlib import pyplot as plt
 
 
-def eval_track(tracked_centroids, filename, location):
+def eval_track(tracked_centroids, time_range, filename, location):
     # use `tracked_centroids`'s frame, x, y, p
     # to overlay the points onto MIP of raw image
 
@@ -44,60 +45,16 @@ def eval_track(tracked_centroids, filename, location):
     # overlay points in each frame on corresponding
     # raw MIP image
     frames = tuple(tracked_centroids.groupby('frame'))
-    time_range = range(len(frames))
     for t, (_, frame) in zip(time_range, frames):
         exists = True  # assume image exists until proven otherwise
         name = f'{t:05d}'
 
         # don't output if it's already there
-        if not os.path.exists(join(location, f'{name}.png')):
+        if True:  # not os.path.exists(join(location, f'{name}.png')):
             exists = False
 
-            data = utils.get_raw_array(
-                path, t, ch=channels).compute()
-
-            cells_mip = np.max(data[:, :, :, channels[0]], axis=0)
-            cells_scale = skimage.exposure.rescale_intensity(
-                cells_mip, in_range=(0, 255), out_range=(220, 255))
-            cells_nl = utils.normalize(cells_scale, 220, 255, cv2.CV_8UC1)
-            cells_nl = cv2.fastNlMeansDenoising(cells_nl, None, 11, 7, 21)
-            # normalize ...
-            # cells_scale = utils.normalize(cells_scale, 200, 255, cv2.CV_8UC1)
-            # cells_rgb = np.zeros((*cells_scale.shape, 3))
-            # cells_rgb[:, :, 1] = cells_scale
-            # cv2.cvtColor(cells_scale.astype(np.float32),
-            #                         cv2.COLOR_GRAY2RGB)
-
-            tubes_mip = np.max(data[:, :, :, channels[1]], axis=0)
-            tubes_mip = utils.normalize(tubes_mip, 0, 255, cv2.CV_8UC1)
-            # tubes_rgb = np.zeros((*tubes_mip.shape, 3))
-            # tubes_rgb[:, :, 2] = tubes_mip
-            # cv2.cvtColor(tubes_mip.astype(np.float32),
-            #                         cv2.COLOR_GRAY2RGB)
-
-            # linear blend between cells and tubes
-            # alpha = 0.90
-            # blended = cv2.addWeighted(cells_nl.astype(np.float32), alpha,
-            #                           tubes_mip.astype(np.float32), 1 - alpha,
-            #                           0, None, dtype=-1)
-
-            tubes_mip = utils.normalize(tubes_mip, 0, 1, cv2.CV_32FC1)
-            cells_nl = utils.normalize(cells_nl, 0, 1, cv2.CV_32FC1)
-
-            combined = np.zeros((1024, 1024, 4), dtype=np.float32)
-            combined[:, :, 0] = cells_nl
-            combined[:, :, 1] = tubes_mip  # np.round(blended).astype(np.uint8)
-            combined[:, :, 2] = 0.
-            combined[:, :, 3] = 1.
-
-            utils.imsave(join(location, f'{name}.png'),
-                         combined, False)
-
-            # output cells and tubes
-            utils.imsave(join(location, f'{name}_cells.png'),
-                         cells_scale, False, 'Reds')
-            utils.imsave(join(location, f'{name}_tubes.png'),
-                         tubes_mip, False, 'viridis')
+            images = output_raw_images(location, path, channels, t, name)
+            combined, cells_scale, tubes_mip = images
 
         X, Y, P, I = (frame[c] for c in columns)
 
@@ -116,29 +73,84 @@ def eval_track(tracked_centroids, filename, location):
 
         save = join(location, tracking_folder, f'{t:05d}.png')
 
-        plt.subplot(131)
-        plt.title('Beta cell channel (0)')
-        plt.imshow(cells_scale, cmap='Reds')
+        output_figure(filename, t, cells_scale, tubes_mip, combined, save)
 
-        plt.subplot(132)
-        plt.imshow(combined, extent=(0, 1024, 1024, 0))
-        plt.xlabel('X dimension')
-        plt.xlim(left=0, right=1024)
-        plt.ylabel('Y dimension')
-        plt.ylim(bottom=1024, top=0)
-        plt.title('Tracked cells with tubes overlay')
 
-        plt.subplot(133)
-        plt.title('Tubes channel (1)')
-        plt.imshow(tubes_mip, cmap='viridis')
+def output_raw_images(location, path, channels, t, name):
+    data = utils.get_raw_array(
+        path, t, ch=channels).compute()
 
-        plt.suptitle(
-            f'Tracked beta cells for\nfile: {filename}, timepoint: {t}',
-            fontsize=30)
+    cells_mip = np.max(data[:, :, :, channels[0]], axis=0)
+    tubes_mip = np.max(data[:, :, :, channels[1]], axis=0)
 
-        plt.tight_layout()
-        plt.savefig(save, dpi=300)
-        plt.close()
+    cells_scale = skimage.exposure.rescale_intensity(
+        cells_mip, in_range=(0, 255), out_range=(220, 255))
+    cells_nl = utils.normalize(cells_scale, 220, 255, cv2.CV_8UC1)
+    cells_nl = cv2.fastNlMeansDenoising(cells_nl, None, 11, 7, 21)
+    tubes_mip = utils.normalize(tubes_mip, 0, 255, cv2.CV_8UC1)
+    tubes_mip = utils.normalize(tubes_mip, 0, 1, cv2.CV_32FC1)
+    cells_nl = utils.normalize(cells_nl, 0, 1, cv2.CV_32FC1)
+    combined = np.zeros((1024, 1024, 4), dtype=np.float32)
+    combined[:, :, 0] = cells_nl
+    combined[:, :, 1] = tubes_mip  # np.round(blended).astype(np.uint8)
+    combined[:, :, 2] = 0.
+    combined[:, :, 3] = 1.
+    # normalize ...
+    # cells_scale = utils.normalize(cells_scale, 200, 255, cv2.CV_8UC1)
+    # cells_rgb = np.zeros((*cells_scale.shape, 3))
+    # cells_rgb[:, :, 1] = cells_scale
+    # cv2.cvtColor(cells_scale.astype(np.float32),
+    #                         cv2.COLOR_GRAY2RGB)
+
+    # tubes_rgb = np.zeros((*tubes_mip.shape, 3))
+    # tubes_rgb[:, :, 2] = tubes_mip
+    # cv2.cvtColor(tubes_mip.astype(np.float32),
+    #                         cv2.COLOR_GRAY2RGB)
+
+    # linear blend between cells and tubes
+    # alpha = 0.90
+    # blended = cv2.addWeighted(cells_nl.astype(np.float32), alpha,
+    #                           tubes_mip.astype(np.float32), 1 - alpha,
+    #                           0, None, dtype=-1)
+
+    utils.imsave(join(location, f'{name}.png'),
+                 combined, False)
+
+    # output cells and tubes
+    utils.imsave(join(location, f'{name}_cells.png'),
+                 cells_scale, False, 'Reds')
+    utils.imsave(join(location, f'{name}_cells_raw.png'),
+                 cells_mip, False, 'viridis')
+    utils.imsave(join(location, f'{name}_tubes.png'),
+                 tubes_mip, False, 'viridis')
+
+    return combined, cells_scale, tubes_mip
+
+
+def output_figure(filename, t, cells_scale, tubes_mip, combined, save):
+    plt.subplot(131)
+    plt.title('Beta cell channel (0) with amplified signal')
+    plt.imshow(cells_scale, cmap='Reds')
+
+    plt.subplot(132)
+    plt.imshow(combined, extent=(0, 1024, 1024, 0))
+    plt.xlabel('X dimension')
+    plt.xlim(left=0, right=1024)
+    plt.ylabel('Y dimension')
+    plt.ylim(bottom=1024, top=0)
+    plt.title('Tracked cells with tubes overlay')
+
+    plt.subplot(133)
+    plt.title('Tubes channel (1)')
+    plt.imshow(tubes_mip, cmap='Greens')
+
+    plt.suptitle(
+        f'Tracked beta cells for\nfile: {filename}, timepoint: {t}',
+        fontsize=30)
+
+    plt.tight_layout()
+    plt.savefig(save, dpi=300)
+    plt.close()
 
 
 def plot_markers(marker_size, colors, X, Y, P, I):
@@ -154,24 +166,27 @@ def plot_markers(marker_size, colors, X, Y, P, I):
 if __name__ == '__main__':
     tic = time()
     utils.setcwd(__file__)
-    # output 2D movie
-    name = c.PRED_FILE
+    files = c.RAW_FILES['test']
+    files = utils.add_ext(files)  # add extensions (.lsm etc.)
+    name = files[0]  # c.PRED_FILE
 
     load_location = join(c.DATA_DIR, c.PRED_DIR,
-                         c.PRED_FILE, 'tracked_centroids.pkl')
+                         name, 'tracked_centroids.pkl')
     tracked_centroids = pickle.load(
         open(load_location, 'rb'))
 
     save_location = join(c.PROJECT_DATA_DIR, c.PRED_DIR,
-                         'eval', 'track_2D', c.PRED_FILE)
+                         'eval', 'track_2D', name)
     utils.make_dir(save_location)
 
-    eval_track(tracked_centroids, c.PRED_FILE, save_location)
+    frames = tuple(pd.unique(tracked_centroids['frame']))
+    time_range = range(min(frames), max(frames))
+    eval_track(tracked_centroids, time_range, name, save_location)
 
-    frames = tuple(tracked_centroids.groupby('frame'))
-    time_range = range(len(frames))
-    plot.create_movie(join(save_location, 'with_tracking'),
-                      time_range)
+    # frames = tuple(tracked_centroids.groupby('frame'))
+    # time_range = range(len(frames))
+    # plot.create_movie(join(save_location, 'with_tracking'),
+    #                   time_range)
 
     elapsed = utils.time_report(tic, time())
     print(f'eval_track finished in {elapsed}.')
