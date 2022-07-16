041c891e2c19e97a27364386e03fdf6d31b77010
Gudmundur <gb9@pm.me>
2022-07-16 11:50:25 +0200

    choose_annotate now acts on all modes (train, val, test)

--- src/data/choose_annotate.py
+++ src/data/choose_annotate.py
@@ -6,41 +6,46 @@ import src.data.constants as c
 import os.path as osp
 import random
 import src.data.utils.utils as utils
-import numpy as np
 
 if __name__ == '__main__':
-    mode = 'train'
+    modes = ('train', 'val', 'test')
     db_version = 'hist_eq'
-    ratio_sample = 0.1  # how much of the dataset to sample
+    sample_ratios = (0.1,) * 3  # how much of the datasets to sample
 
     random.seed(42)
     utils.set_cwd(__file__)
     tic = time()
     root_dir = c.DATA_DIR if osp.exists(c.DATA_DIR) else c.PROJECT_DATA_DIR
 
-    load_path = osp.join(root_dir, mode,
-                         c.IMG_DIR, db_version)
-    sample_dir = osp.join(load_path, 'sampled_for_labeling')
-    utils.make_dir(sample_dir)
+    for mode, sample_ratio in zip(modes, sample_ratios):
+        load_path = osp.join(root_dir, db_version,
+                             mode, c.IMG_DIR)
+        sample_dir = osp.join(root_dir, db_version, mode,
+                              c.MASK_DIR)
+        utils.make_dir(sample_dir)
 
-    images = sorted((image for image in os.listdir(load_path)
-                     if '.png' in image))
-    already_sampled = sorted([image for image in os.listdir(sample_dir)])
-    # remove images which have already been sampled
-    images = [image for image in images
-              if image not in already_sampled]
+        images = sorted((image for image in os.listdir(load_path)
+                        if '.png' in image))
+        already_sampled = sorted([image
+                                  for image in os.listdir(sample_dir)
+                                  if '.png' in image])
 
-    k = int(len(images) * ratio_sample) - len(already_sampled)
-    if k <= 0:
-        print(('With the chosen ratio,'
-               'no more images can be sampled as there are'
-               'already more than enough sampled images.'))
-        exit()
+        # remove images which have already been sampled
+        images = [image for image in images
+                  if image not in already_sampled]
 
-    sampled_imgs = random.sample(images, k=k)
+        k = int(len(images) * sample_ratio) - len(already_sampled)
+        if k <= 0:
+            print((f'With the chosen ratio,'
+                   f'no more images can be sampled from {mode}'
+                   f'dataset as there are'
+                   f'already more than enough sampled images.'))
+            continue
 
-    for sample in sampled_imgs:
-        sampled_img = plt.imread(osp.join(load_path, sample))
-        utils.imsave(osp.join(sample_dir, sample), sampled_img)
+        sampled_imgs = random.sample(images, k=k)
+
+        for sample in sampled_imgs:
+            sampled_img = plt.imread(osp.join(load_path, sample))
+            utils.imsave(osp.join(sample_dir, sample), sampled_img)
 
     print(f'choose_annotate.py completed in {utils.time_report(tic, time())}.')
