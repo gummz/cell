a520c59143c32094682cb20b8fa985369a66ead0
Gudmundur <gb9@pm.me>
2022-08-24 23:24:14 +0200

    Minor change to get_pr_Trajs

--- src/tracking/eval_track.py
+++ src/tracking/eval_track.py
@@ -83,10 +83,10 @@ def evaluate(pred_dir_path, json_files, cell_images,
 
 
 def get_pr_trajs(pred_dir_path, timepoints, groupby='particle'):
-    time_range = range(min(timepoints), max(timepoints) + 1)
     track_path = osp.join(pred_dir_path, 'tracked_centroids.csv')
     tracked_centroids = pd.read_csv(track_path, header=0, sep=';')
     if timepoints:
+        time_range = range(min(timepoints), max(timepoints) + 1)
         tc_filter = tracked_centroids[tracked_centroids['frame'].isin(
             time_range)]
     else:
