b7794b7cfd21b69ade37c603ff12955ae5ac5d15
gummz <gb9@pm.me>
2022-09-05 22:39:39 +0200

    Remove profiler from predict_model.sh

--- shell/predict_model.sh
+++ shell/predict_model.sh
@@ -48,4 +48,4 @@ source ../venv_1/bin/activate
 ###python3 -m pip --disable-pip-version-check install pycocotools
 
 # Run file
-python3 -m cProfile -s tottime ../src/models/predict_model.py > pyout_predict
+python3 ../src/models/predict_model.py > pyout_predict
