ce897043c2b986508388a21eb77913131115ddb0
gummz <gb9@pm.me>
2022-10-02 17:51:17 +0200

    Formatting fixes

--- src/tracking/fraction_in_loop.py
+++ src/tracking/fraction_in_loop.py
@@ -9,9 +9,9 @@ import src.data.utils.utils as utils
 
 def born_in_loop(iterable, first):
     start = np.array(
-            [cell[1].iloc[:first].in_loop
-            for cell in iterable]
-        )
+        [cell[1].iloc[:first].in_loop
+         for cell in iterable]
+    )
 
     cells_inloop = np.sum(np.any(start > 0, axis=1)) / len(start)
     cells_outloop = np.sum(np.all(start == 0, axis=1)) / len(start)
@@ -19,16 +19,16 @@ def born_in_loop(iterable, first):
     return cells_inloop, cells_outloop
 
 
-def compare_dist(iterable1, iterable0, 
-first):
+def compare_dist(iterable1, iterable0,
+                 first):
     dist_beta = np.array(
-            [cell[1].iloc[:first].dist_loop
-                for cell in iterable1]
-        )
+        [cell[1].iloc[:first].dist_loop
+         for cell in iterable1]
+    )
     dist_nonbeta = np.array(
-            [cell[1].iloc[:first].dist_loop
-            for cell in iterable0]
-        )
+        [cell[1].iloc[:first].dist_loop
+         for cell in iterable0]
+    )
 
     mean_beta = dist_beta.mean(axis=1).mean()
     mean_nonbeta = dist_nonbeta.mean(axis=1).mean()
@@ -42,26 +42,26 @@ if __name__ == '__main__':
                         experiment_name)
 
     utils.set_cwd(__file__)
-    pattern = osp.join(pred_dir, '*', 
-    'tracked_centroids_loops_t*.csv')
+    pattern = osp.join(pred_dir, '*',
+                       'tracked_centroids_loops_t*.csv')
     pred_files = glob.glob(pattern, recursive=True)
-    
+
     names = pd.Series([osp.basename(osp.dirname(pred_path))
-            for pred_path in pred_files])
+                       for pred_path in pred_files])
     values = pd.DataFrame(np.empty((len(pred_files), 12), dtype=np.float32))
     columns = [
-    'filename', 
-    'cells_inloop0', 'cells_outloop0',
-    'cells_inloop5', 'cells_outloop5',
-    'mean_beta01', 'mean_nonbeta01',
-    'mean_beta51', 'mean_nonbeta51',
-    'mean_beta02', 'mean_nonbeta02',
-    'mean_beta52', 'mean_nonbeta52'
+        'filename',
+        'cells_inloop0', 'cells_outloop0',
+        'cells_inloop5', 'cells_outloop5',
+        'mean_beta01', 'mean_nonbeta01',
+        'mean_beta51', 'mean_nonbeta51',
+        'mean_beta02', 'mean_nonbeta02',
+        'mean_beta52', 'mean_nonbeta52'
     ]
     for i, (name, pred_path) in enumerate(zip(names, pred_files)):
         pred_file = pd.read_csv(pred_path, sep=';')
-        iterable1 = pred_file[pred_file.beta==1].groupby('particle')
-        iterable0 = pred_file[pred_file.beta==0].groupby('particle')
+        iterable1 = pred_file[pred_file.beta == 1].groupby('particle')
+        iterable0 = pred_file[pred_file.beta == 0].groupby('particle')
 
         cells_inloop0, cells_outloop0 = born_in_loop(iterable1, 1)
         cells_inloop5, cells_outloop5 = born_in_loop(iterable1, 5)
@@ -70,20 +70,20 @@ if __name__ == '__main__':
         mean_beta51, mean_nonbeta51 = compare_dist(iterable1, iterable0, 5)
 
         pred_file.dist_loop = np.where(
-        ((pred_file.in_loop > 0) & (pred_file.beta == 1)), 
-        pred_file.dist_loop, 0) # reverse pred_file.dist_loop and 0?
-        iterable1 = pred_file[pred_file.beta==1].groupby('particle')
-        iterable0 = pred_file[pred_file.beta==0].groupby('particle')
+            ((pred_file.in_loop > 0) & (pred_file.beta == 1)),
+            pred_file.dist_loop, 0)  # reverse pred_file.dist_loop and 0?
+        iterable1 = pred_file[pred_file.beta == 1].groupby('particle')
+        iterable0 = pred_file[pred_file.beta == 0].groupby('particle')
         mean_beta02, mean_nonbeta02 = compare_dist(iterable1, iterable0, 1)
         mean_beta52, mean_nonbeta52 = compare_dist(iterable1, iterable0, 5)
 
         entry = np.round((
-                cells_inloop0, cells_outloop0,
-                cells_inloop5, cells_outloop5,
-                mean_beta01, mean_nonbeta01,
-                mean_beta51, mean_nonbeta51,
-                mean_beta02, mean_nonbeta02,
-                mean_beta52, mean_nonbeta52), 3)
+            cells_inloop0, cells_outloop0,
+            cells_inloop5, cells_outloop5,
+            mean_beta01, mean_nonbeta01,
+            mean_beta51, mean_nonbeta51,
+            mean_beta02, mean_nonbeta02,
+            mean_beta52, mean_nonbeta52), 3)
 
         values.iloc[i] = entry
 
